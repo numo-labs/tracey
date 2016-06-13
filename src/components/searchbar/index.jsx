692afc6e40579a41d9ad70ee1e44041791371799
jruts <jimmy.ruts@thomascookonline.com>
2016-06-13 15:37:17 +0100

    Switch between environments

--- src/components/searchbar/index.jsx
+++ src/components/searchbar/index.jsx
@@ -1,6 +1,10 @@
 import React, { PropTypes } from 'react';
 
+import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import Button from 'react-bootstrap/lib/Button';
+import DropdownButton from 'react-bootstrap/lib/DropdownButton';
+import SplitButton from 'react-bootstrap/lib/SplitButton';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
 import Form from 'react-bootstrap/lib/Form';
 import FormControl from 'react-bootstrap/lib/FormControl';
 
@@ -11,21 +15,27 @@ const propTypes = {
 class Searchbar extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {search_field_value: ''};
+    this.state = {search_field_value: '', env: 'ci'};
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handleEnterOnInputField = this.handleEnterOnInputField.bind(this);
+    this.handleChangeEnvironment = this.handleChangeEnvironment.bind(this);
   }
 
   handleSubmit () {
-    this.props.onSubmit(this.state.search_field_value);
+    this.props.onSubmit(this.state.search_field_value, this.state.env);
   }
 
   handleEnterOnInputField (event) {
+    console.log(event);
     if (event.key === 'Enter') {
       event.preventDefault();
       this.handleSubmit();
     }
+  }
+
+  handleChangeEnvironment (env) {
+    this.setState({env: env});
   }
 
   handleTextChange (event) {
@@ -36,7 +46,14 @@ class Searchbar extends React.Component {
     return (
       <Form inline>
         <FormControl style={{width: '300px'}} type='text' onChange={this.handleTextChange} onKeyPress={this.handleEnterOnInputField} value={this.state.search_field_value} />{' '}
-        <Button onClick={this.handleSubmit}>Search</Button>
+        <SplitButton title={'Search ' + this.state.env} key={1} id='search-in-environment-button' onClick={this.handleSubmit}>
+          <MenuItem eventKey='1' onClick={() => {
+            const env = this.state.env === 'ci' ? 'prod' : 'ci';
+            this.handleChangeEnvironment(env);
+          }}>
+            {this.state.env === 'ci' ? 'Switch to prod' : 'Switch to ci'}
+          </MenuItem>
+        </SplitButton>
       </Form>
     );
   }
