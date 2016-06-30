5f49a3355834ba8d4a7d080ef731cede7680cf1c
Tom Gallacher <tomgallacher23@gmail.com>
2016-06-30 15:07:18 +0100

    Add queries to general information tab

--- src/components/overviewPage/index.jsx
+++ src/components/overviewPage/index.jsx
@@ -5,9 +5,14 @@ import Panel from 'react-bootstrap/lib/Panel';
 const D3Panel = ({results, params}) => {
   console.log('changeing');
   return (
-    <Panel header='Graph'>
-      <ForceLayout width={700} height={500} results={results} />
-    </Panel>
+    <div>
+      <Panel header='Queries'>
+        {results.queries.map(obj => <pre><code>{obj}</code></pre>)}
+      </Panel>
+      <Panel header='Graph'>
+        <ForceLayout width={700} height={500} results={results} />
+      </Panel>
+    </div>
   );
 };
 
